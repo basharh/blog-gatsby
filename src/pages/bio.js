3b06c9e64f81548d0afa9c42fcffb34c19ca1ba1
Bashar Harfoush <bashar.harfoush@gmail.com>
2020-03-10 01:52:40 -0400

    add bio page

---
+++ src/pages/bio.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const Bio = () => (
+  <Layout>
+    <SEO title="About Me" />
+    <div>A bit about the author here!</div>
+  </Layout>
+);
+
+export default Bio;

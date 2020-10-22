2d591e7d5b021a3e977f00471c6059659b449fb9
Bashar Harfoush <bashar.harfoush@gmail.com>
2020-10-22 16:21:56 -0400

    update bio

--- src/pages/bio.tsx
+++ src/pages/bio.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { css } from "@emotion/core";
-import PropTypes from "prop-types";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const Bio = ({ data }) => (
+interface GithubFile {
+  childMarkdownRemark: {
+    html: string;
+  };
+}
+
+const Bio: React.FC<{ data: { ar: GithubFile; en: GithubFile } }> = ({
+  data: { ar, en },
+}) => (
   <Layout>
     <SEO title="About Me" />
     <div
@@ -27,7 +34,7 @@ const Bio = ({ data }) => (
         `}
       >
         <div
-          dangerouslySetInnerHTML={{ __html: data.ar.childMarkdownRemark.html }}
+          dangerouslySetInnerHTML={{ __html: ar.childMarkdownRemark.html }}
         />
       </div>
       <div
@@ -37,29 +44,20 @@ const Bio = ({ data }) => (
             line-height: 1.3;
           }
         `}
-        dangerouslySetInnerHTML={{ __html: data.en.childMarkdownRemark.html }}
+        dangerouslySetInnerHTML={{ __html: en.childMarkdownRemark.html }}
       />
     </div>
   </Layout>
 );
 
-Bio.propTypes = {
-  data: PropTypes.shape({
-    ar: PropTypes.string,
-    en: PropTypes.string,
-  }).isRequired,
-};
-
 export const query = graphql`
   query {
     ar: githubFile(relativePath: { eq: "bio/ar.md" }) {
-      base
       childMarkdownRemark {
         html
       }
     }
     en: githubFile(relativePath: { eq: "bio/en.md" }) {
-      base
       childMarkdownRemark {
         html
       }
